5d4d1a4e23274c14ccf9a8dec47763db9b76ef47
Naheem Johnson Solís <naheemjohnson@estudiantec.cr>
2021-06-13 11:45:42 -0600

    Added basis classes to the project

--- src/Objects/TypeMessage.h
+++ src/Objects/TypeMessage.h
@@ -5,4 +5,14 @@
 #ifndef TEC_FS_TYPEMESSAGE_H
 #define TEC_FS_TYPEMESSAGE_H
 
+#include <iostream>
+#include "../DataStructures/SimplyList.h"
+
+using namespace std;
+
+class TypeMessage
+{
+
+};
+
 #endif //TEC_FS_TYPEMESSAGE_H
--- src/UtilJSON/JSON_Management.h
+++ src/UtilJSON/JSON_Management.h
@@ -5,4 +5,19 @@
 #ifndef TEC_FS_JSON_MANAGEMENT_H
 #define TEC_FS_JSON_MANAGEMENT_H
 
+#include <iostream>
+#include "../../lib/rapidjson/stringbuffer.h"
+#include "../../lib/rapidjson/stringbuffer.h"
+#include "../../lib/rapidjson/writer.h"
+#include "../../lib/rapidjson/document.h"
+#include "../DataStructures/SimplyList.h"
+#include "../Objects/TypeMessage.h"
+
+using namespace rapidjson;
+using namespace std;
+
+class JSON_Management
+{
+
+};
 #endif //TEC_FS_JSON_MANAGEMENT_H
