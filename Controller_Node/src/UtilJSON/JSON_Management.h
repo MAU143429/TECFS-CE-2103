a5863960473590e76abca4c18b0ea76a5608b400
Naheem Johnson Solís <naheemjohnson@estudiantec.cr>
2021-06-14 12:47:38 -0600

    Changed location of rapidjson library

--- Controller_Node/src/UtilJSON/JSON_Management.h
+++ Controller_Node/src/UtilJSON/JSON_Management.h
@@ -6,10 +6,10 @@
 #define TEC_FS_JSON_MANAGEMENT_H
 
 #include <iostream>
-#include "../../lib/rapidjson/stringbuffer.h"
-#include "../../lib/rapidjson/stringbuffer.h"
-#include "../../lib/rapidjson/writer.h"
-#include "../../lib/rapidjson/document.h"
+#include "../../../lib/rapidjson/stringbuffer.h"
+#include "../../../lib/rapidjson/stringbuffer.h"
+#include "../../../lib/rapidjson/writer.h"
+#include "../../../lib/rapidjson/document.h"
 #include "../DataStructures/SimplyList.h"
 #include "../Objects/TypeMessage.h"
 
@@ -18,6 +18,20 @@ using namespace std;
 
 class JSON_Management
 {
+public:
+    static string TypeMessageToJSON(TypeMessage* message){
+        Document json_document;
+        StringBuffer buffer;
 
+        Document::AllocatorType& allocator = json_document.GetAllocator();
+
+        Writer<rapidjson::StringBuffer> writer(buffer);
+
+        writer.StartObject();
+
+        writer.EndObject();
+
+        return buffer.GetString();
+    }
 };
 #endif //TEC_FS_JSON_MANAGEMENT_H
