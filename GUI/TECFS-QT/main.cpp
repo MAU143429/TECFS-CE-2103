b166976a0ec6df1a74dfcfd9dd6382c62d0305a0
Keyner <keyner.gp.steff@gmail.com>
2021-06-16 00:38:05 -0600

    Added client connection to the gui

--- GUI/TECFS-QT/main.cpp
+++ GUI/TECFS-QT/main.cpp
@@ -1,11 +1,26 @@
-#include "mainwindow.h"
-
+#include "src/MainWindow/mainwindow.h"
+#include "src/Socket/Client.cpp"
+#include <thread>
 #include <QApplication>
 
-int main(int argc, char *argv[])
+void RunClient() {
+    InitClient();
+}
+int runGui(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
 }
+
+
+int main(int argc, char *argv[])
+{
+    thread runs(runGui,argc,argv);
+    thread main(RunClient);
+    runs.join();
+    main.join();
+    return 0;
+}
+
--- GUI/TECFS-QT/src/MainWindow/mainwindow.cpp
+++ GUI/TECFS-QT/src/MainWindow/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "cerobot.h"
-#include "cesearch.h"
+#include "src/ceRobot/cerobot.h"
+#include "src/ceSearch/cesearch.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
