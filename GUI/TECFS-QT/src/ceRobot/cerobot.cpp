bf4a9f5668c4846a124c7310c50802affc1f573b
Keyner <keyner.gp.steff@gmail.com>
2021-06-17 23:50:49 -0600

    GUI functions update

--- GUI/TECFS-QT/src/ceRobot/cerobot.cpp
+++ GUI/TECFS-QT/src/ceRobot/cerobot.cpp
@@ -3,6 +3,7 @@
 #include <QFileDialog>
 #include "../Objects/TypeMessage.h"
 #include "../UtilJSON/JSON_Management.h"
+#include "../Algorithms/HuffmanCompression.h"
 
 ceROBOT::ceROBOT(QWidget *parent) :
     QMainWindow(parent),
@@ -21,20 +22,29 @@ ceROBOT::~ceROBOT()
 void ceROBOT::on_uploadBtn_clicked()
 {
     QString filePath = QFileDialog::getExistingDirectory(this, "Select a Directory");
+    ui->pathLine->setText(filePath);
 }
 
 //Funcion del boton para subir al server.
 void ceROBOT::on_pushBtn_clicked()
 {
-    if( true){
-
+    if(ui->pathLine->text() == "Direccion de la carpeta"){
+        ui->statusLabel->setText("Error, debes cargar una carpeta.");
     }else{
         auto robotMsg = new TypeMessage();
         robotMsg->setAppName("CEROBOT");
-        robotMsg->setPath("");
+        robotMsg->setPath(ui->pathLine->text().toStdString());
         robotMsg->setClientType("APP");
 
         string jsonMsg = JSON_Management::TypeMessageToJSON(robotMsg);
+        pair<string,SimplyLinkedList<Huffman_pair*>*> compressed;
+        compressed = HuffmanCompression::buildHuffmanTree(jsonMsg);
+        auto huffmanMsg = new Huffman_Message();
+        huffmanMsg->setCompress_Code(compressed.first);
+        for (int i = 0; i < compressed.second->getLen(); ++i) {
+            huffmanMsg->getHuffman_Table()->append(compressed.second->get(i));
+        }
+        string finalMsg = JSON_Management::HuffmanMessageToJSON(huffmanMsg);
 
     }
 }
--- GUI/TECFS-QT/src/ceSearch/cesearch.cpp
+++ GUI/TECFS-QT/src/ceSearch/cesearch.cpp
@@ -3,6 +3,7 @@
 #include <QMessageBox>
 #include "../Objects/TypeMessage.h"
 #include "../UtilJSON/JSON_Management.h"
+#include "../Algorithms/HuffmanCompression.h"
 
 ceSEARCH::ceSEARCH(QWidget *parent) :
     QMainWindow(parent),
@@ -27,7 +28,7 @@ void ceSEARCH::on_backBtn_clicked()
 void ceSEARCH::on_searchBtn_clicked()
 {
     if(ui->searchBar->text() == nullptr or ui->searchBar->text() == ""){
-        ui->resultLabel->setText("error");
+        ui->resultLabel->setText("Error, debes escribir una palabra en la barra de busqueda.");
     }else{
         auto searchMsg = new TypeMessage();
         searchMsg->setAppName("CESEARCH");
@@ -35,7 +36,13 @@ void ceSEARCH::on_searchBtn_clicked()
         searchMsg->setClientType("APP");
 
         string jsonMsg = JSON_Management::TypeMessageToJSON(searchMsg);
-        pair<string,SimplyLinkedList<Huffman_pair>> compressed;
-        compressed = ;
+        pair<string,SimplyLinkedList<Huffman_pair*>*> compressed;
+        compressed = HuffmanCompression::buildHuffmanTree(jsonMsg);
+        auto huffmanMsg = new Huffman_Message();
+        huffmanMsg->setCompress_Code(compressed.first);
+        for (int i = 0; i < compressed.second->getLen(); ++i) {
+            huffmanMsg->getHuffman_Table()->append(compressed.second->get(i));
+        }
+        string finalMsg = JSON_Management::HuffmanMessageToJSON(huffmanMsg);
     }
 }
