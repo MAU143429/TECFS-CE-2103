a26b9c7aed56ef2510ed014433511b89725dbea7
Mauricio Calderon <mau1412126@gmail.com>
2021-06-17 22:29:58 -0600

    App and DIsk Controllers added

---
+++ Controller_Node/src/DiskNodes_Controller/Disk_Controller.h
@@ -0,0 +1,24 @@
+//
+// Created by mau on 6/17/21.
+//
+
+#ifndef TEC_FS_DISK_CONTROLLER_H
+#define TEC_FS_DISK_CONTROLLER_H
+
+
+using namespace std:
+
+
+class Disk_Controller{
+
+public:
+
+    static void Controller_Disk(const string& jsonString){
+
+
+
+    }
+
+
+};
+#endif //TEC_FS_DISK_CONTROLLER_H
--- Disk_Node/src/Objects/Huffman_Message.h
+++ Disk_Node/src/Objects/Huffman_Message.h
@@ -4,5 +4,29 @@
 
 #ifndef DISK_NODE_HUFFMAN_MESSAGE_H
 #define DISK_NODE_HUFFMAN_MESSAGE_H
+#include <string>
+#include"../Objects/Huffman_pair.h"
+using namespace std;
 
+
+class Huffman_Message
+{
+private:
+    SimplyLinkedList<Huffman_pair*> *huffman_table = new SimplyLinkedList<Huffman_pair*>();
+    string compress_code;
+public:
+
+    const string& getCompress_Code() const {
+        return compress_code;
+    }
+
+    void setCompress_Code(const string& newcode) {
+        compress_code = newcode;
+    }
+
+    SimplyLinkedList<Huffman_pair*>* getHuffman_Table() {
+        return huffman_table;
+    }
+
+};
 #endif //DISK_NODE_HUFFMAN_MESSAGE_H
