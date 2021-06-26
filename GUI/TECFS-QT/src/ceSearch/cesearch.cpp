07ee3599e236584156053d6f76449ba6f5dcefe6
Keyner <keyner.gp.steff@gmail.com>
2021-06-26 15:22:37 -0600

    GUI update

--- GUI/TECFS-QT/src/ceSearch/cesearch.cpp
+++ GUI/TECFS-QT/src/ceSearch/cesearch.cpp
@@ -32,11 +32,11 @@ void ceSEARCH::on_searchBtn_clicked()
     if(ui->searchBar->text() == nullptr or ui->searchBar->text() == ""){
         ui->resultLabel->setText("Error, debes escribir una palabra en la barra de busqueda.");
     }else{
-        auto searchMsg = new TypeMessage();
+
+        /**auto searchMsg = new TypeMessage();
         searchMsg->setAppName("CESEARCH");
         searchMsg->setKeyWord(ui->searchBar->text().toStdString());
         searchMsg->setClientType("APP");
-
         string jsonMsg = JSON_Management::TypeMessageToJSON(searchMsg);
         Client::getInstance()->Send(jsonMsg.c_str());
 
@@ -45,7 +45,6 @@ void ceSEARCH::on_searchBtn_clicked()
             response = Client::getInstance()->ReadString();
         }
         Client::getInstance()->setResponse("");
-
         auto huffmanMessage = new Huffman_Message();
         huffmanMessage = JSON_Management::DeserializetoHuffmanMessage(response);
         string msg = HuffmanCompression::Decode_Huffman(huffmanMessage->getCompress_Code(),huffmanMessage->getHuffman_Table());
@@ -62,9 +61,21 @@ void ceSEARCH::on_searchBtn_clicked()
         for (int i = 0; i < blockBools->getLen(); ++i) {
             if(blockBools->get(i) == true){
                 string title = JSON_Management::GetJSONString("KW_B"+to_string(i),response);
-                //Agregar al listview
+
             }
-        }
+        }*/
+
+        ui->listWidget->addItem("libro 1");
+        ui->listWidget->addItem("libro 2");
+        ui->listWidget->addItem("libro 3");
+        ui->listWidget->addItem("libro 4");
+        ui->listWidget->addItem("libro 5");
 
     }
+}
+
+void ceSEARCH::on_listWidget_itemDoubleClicked(QListWidgetItem *item)
+{
+    cout << item->text().toStdString() << "\n";
+    ui->resultLabel->setText("hizo click");
 }
